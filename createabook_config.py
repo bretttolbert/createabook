94f816348936f1b602e03ea55b8b47baf7fba6df
U-Brett-PC\Brett <Brett@Brett-PC.(none)>
2014-06-28 09:28:45 -0500

    updated config comments

--- createabook_config.py
+++ createabook_config.py
@@ -1,9 +1,15 @@
-#all fields are required, but empty is a valid value
+#All fields are required, but empty is a valid value, which means a 
+#form field will be displayed for that input.
+#The kindle email form field is always displayed, but you can specify
+#a default kindle email which is used as the default value of the form field.
+#For the other inputs, if you define set the value here, the form field will
+#not be displayed.
 DEFAULT_KINDLE_EMAIL = 'user@kindle.com'   # Kindle email address (e.g. 'mykindle@amazon.com')
-FROM_EMAIL = 'user@gmail.com'     # This email must be in your Amazon Kindle approved sender list
-SMTP_SERVER = 'smtp.gmail.com:587' # SMTP server for FROM_EMAIL account
-SMTP_USERNAME = 'user'  # SMTP username for FROM_EMAIL account
-SMTP_PASSWORD = 'secret'  # SMTP password for FROM_EMAIL account
+FROM_EMAIL = ''     # This email must be in your Amazon Kindle approved sender list
+SMTP_SERVER = '' # SMTP server for FROM_EMAIL account e.g. smtp.gmail.com:587
+SMTP_USERNAME = ''  # SMTP username for FROM_EMAIL account
+SMTP_PASSWORD = ''  # SMTP password for FROM_EMAIL account
 EXTERNALY_VISIBLE_SERVER = True
+#Credentials for HTTP Basic Auth
 HTTP_AUTH_LOGIN = 'admin'
 HTTP_AUTH_PASSWD = 'secret'
