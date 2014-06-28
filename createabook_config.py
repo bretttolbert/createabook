1ecd3f63275c1180d9661247eba6022dd53fe711
U-Brett-PC\Brett <Brett@Brett-PC.(none)>
2014-06-28 07:36:09 -0500

    added HTTP basic auth and external config file

---
+++ createabook_config.py
@@ -0,0 +1,8 @@
+KINDLE_EMAIL = ''   # Kindle email address (e.g. 'mykindle@amazon.com')
+FROM_EMAIL = ''     # This email must be in your Amazon Kindle approved sender list
+SMTP_SERVER = 'smtp.gmail.com:587' # SMTP server for FROM_EMAIL account
+SMTP_USERNAME = ''  # SMTP username for FROM_EMAIL account
+SMTP_PASSWORD = ''  # SMTP password for FROM_EMAIL account
+EXTERNALY_VISIBLE_SERVER = True
+HTTP_AUTH_LOGIN = 'admin'
+HTTP_AUTH_PASSWD = 'secret'
